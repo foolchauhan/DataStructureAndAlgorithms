c4970f6d2c92049362a3041b83b7109718f320a1
Chetan Chauhan <chauhanchetan82@gmail.com>
2020-05-04 14:18:02 +0530

    LeetCode May challenge : Day 4 : Complement Number

---
+++ LeetCode_May_Challenge/Day_4_Complement_Number.py
@@ -0,0 +1,6 @@
+import math
+class Solution:
+    def findComplement(self, num: int) -> int:
+        number_of_bits = (int)(math.floor(math.log(num) / math.log(2))) + 1
+        return ((1 << number_of_bits) - 1) ^ num
+        
