14ad37a7b12aea0b488baef341e6fedd290eff90
Chetan Chauhan <chauhanchetan82@gmail.com>
2020-05-12 16:22:29 +0530

    LeetCode May Challenge : Day 12 : Single Number in a Sorted Array

---
+++ LeetCode_May_Challenge/Day_12_Single_Number_in_a_Sorted_Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums) - 1
+        while True:
+            mid = (low + high) // 2
+            if mid % 2 == 1:
+                mid -= 1
+            if mid < high and nums[mid] == nums[mid+1]:
+                low = mid + 2
+            elif mid > low and nums[mid] == nums[mid-1]:
+                high = mid - 2
+            else:
+                return nums[mid]
+
+
